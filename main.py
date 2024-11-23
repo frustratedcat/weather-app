49ff588e6bc489b7146a4b4ab63c29f8cc65bf66
frustratedcat <brownjared205@gmail.com>
2024-11-23 13:43:35 -0500

    Finish everything for now

--- main.py
+++ main.py
@@ -9,8 +9,11 @@ def clear_screen():
 
 # Connect to webdriver
 clear_screen()
-print('Starting Up\n')
-driver = webdriver.Firefox()
+print('Starting Up. Please Wait...\n')
+options = webdriver.FirefoxOptions()
+options.add_argument('-headless')
+options.page_load_strategy = 'eager'
+driver = webdriver.Firefox(options=options)
 driver.get('https://www.accuweather.com/')
 
 # Get HTML elements
@@ -25,27 +28,33 @@ def get_user_location():
 
 # Search location and return list of locations or result
 def search_user_location():
-    get_element(20, By.CLASS_NAME, 'search-input').send_keys(get_user_location() + Keys.ENTER)
+    get_element(5, By.CLASS_NAME, 'search-input').send_keys(get_user_location() + Keys.ENTER)
 
 # Current weather result
 def weather_result():
-    current_date = get_element(20, By.XPATH, '//div[contains(@class, "card-header")]/p')
-    current_time = get_element(20, By.XPATH, '//p[contains(@class, "cur-con-weather-card__subtitle")]')
+    current_date = get_element(5, By.XPATH, '//div[contains(@class, "card-header")]/p')
+    current_time = get_element(5, By.XPATH, '//p[contains(@class, "cur-con-weather-card__subtitle")]')
     print(current_date.text, current_time.text)
 
-    today_forecast = get_element(20, By.XPATH, '//div[contains(@class, "body-item")][1]')
-    tonight_forecast = get_element(20, By.XPATH, '//div[contains(@class, "body-item")][2]')
+    today_forecast = get_element(5, By.XPATH, '//div[contains(@class, "body-item")][1]')
+    tonight_forecast = get_element(5, By.XPATH, '//div[contains(@class, "body-item")][2]')
     print(f'\nToday\'s Forecast: {today_forecast.text}\nTonight\'s Forecast: {tonight_forecast.text}')
 
-    current_temp = get_element(20, By.XPATH, '//div[contains(@class, "temp-container")]/div[1]')
-    real_feel = get_element(20, By.XPATH, '//div[contains(@class, "temp-container")]/div[2]')
+    current_temp = get_element(5, By.XPATH, '//div[contains(@class, "temp-container")]/div[1]')
+    real_feel = get_element(5, By.XPATH, '//div[contains(@class, "temp-container")]/div[2]')
     print(f'\nCurrent Temperature: {current_temp.text} {real_feel.text}')
 
-
 def main():
-    search_user_location()
-    clear_screen()
-    weather_result()
+    try:
+        clear_screen()
+        search_user_location()
+        clear_screen()
+        weather_result()
+    except:
+        clear_screen()
+        print('You mistyped, run the application again...')
+    finally:
+        driver.close()
 
 if __name__ == '__main__':
     main()
