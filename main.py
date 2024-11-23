51dabc33c0492ae3a2d83a6771df8f67db5d0d0c
frustratedcat <brownjared205@gmail.com>
2024-11-23 13:12:23 -0500

    Update stuff

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def clear_screen():
 
 # Connect to webdriver
 clear_screen()
+print('Starting Up\n')
 driver = webdriver.Firefox()
 driver.get('https://www.accuweather.com/')
 
@@ -20,15 +21,25 @@ def get_element(time, type, name):
 
 # Get location for search
 def get_user_location():
-    location = input('Type your address here:\n> ')
+    location = input('Type your full address here:\n> ')
     return location.strip()
 
 # Search location and return list of locations or result
 def search_user_location():
     get_element(20, By.CLASS_NAME, 'search-input').send_keys(get_user_location() + Keys.ENTER)
 
+# Current weather result
+def weather_result():
+    today_forecast = get_element(20, By.XPATH, '//div[contains(@class, "body-item")][1]')
+    tonight_forecast = get_element(20, By.XPATH, '//div[contains(@class, "body-item")][2]')
+    print(f'Today\'s Forecast: {today_forecast.text}\nTonight\'s Forecast: {tonight_forecast.text}')
+
+
+
 def main():
     search_user_location()
+    clear_screen()
+    weather_result()
 
 if __name__ == '__main__':
     main()
